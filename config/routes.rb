45b7ce93e1201bc945fb10dbe82329cad5821efb
Danny Chang <dannychachang@gmail.com>
2018-01-10 14:07:32 -0500

    add many kittens and DRY up controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/'             => 'pages#welcome'
   get '/contest'      => 'pages#contest'
   get '/kitten/:size' => 'pages#kitten'
+  get '/kittens/:size'=> 'pages#kittens'
 end
