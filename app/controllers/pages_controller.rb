169506a4ddf835045bd5967b0f4e5c57cb30a4e8
Danny Chang <dannychachang@gmail.com>
2018-01-10 14:00:20 -0500

    Add kitten dynamicl url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "blahblahblah"
     render :welcome
+
   end
 
   def contest
+    @header = "check this out"
     render :contest
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+    @header = "why this no work"
+    render :kitten
   end
 
 end
